a27ce4686a4d436d926814aa1eb2f8762bcc9929
jdinovo <43574189+jdinovo@users.noreply.github.com>
2022-02-28 15:18:20 -0500

    typo

--- src/main/java/gui/partials/CardReceivedView.java
+++ src/main/java/gui/partials/CardReceivedView.java
@@ -3,7 +3,7 @@ package gui.partials;
 /**
  * @author James DiNovo
  *
- * View for displaying card that has been drawn to user
+ * View for displaying card that has been dealt to user
  * and allowing the user to make decisions
  * i.e if deck full discard or play the card
  */
