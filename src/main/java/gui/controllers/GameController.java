2cbef7c576ded9420228f494479969c7ddaf461e
jdinovo <43574189+jdinovo@users.noreply.github.com>
2022-03-04 17:39:14 -0500

    Adding player cards to GUI - Player's game GUI can now be disabled when it is not their turn

--- src/main/java/gui/controllers/GameController.java
+++ src/main/java/gui/controllers/GameController.java
@@ -2,18 +2,15 @@ package gui.controllers;
 
 import game.components.card.AllyCard;
 import game.components.card.Card;
-import game.components.card.QuestCard;
 import gui.panes.GamePane;
 import gui.partials.CardView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.image.Image;
 import model.GameCommand;
 import model.Player;
 import networking.client.Client;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  * @author James DiNovo
@@ -26,6 +23,11 @@ public class GameController {
     private ObservableList<CardView> discarded;
 
     public GameController (GamePane view) {
+        myHand = FXCollections.observableArrayList();
+        view.getMyHand().setListViewItems(myHand);
+
+        discarded = FXCollections.observableArrayList();
+        view.getDiscardedCards().setListViewItems(discarded);
         setView(view);
     }
 
@@ -41,53 +43,37 @@ public class GameController {
             System.out.println(player);
 
             // Add player cards to gui cards
-            ArrayList<Card> cards = (ArrayList<Card>) player.getCards();
+            for (Card card : player.getCards()) {
+                addCardToHand(myHand, card);
+            }
 
         } catch(IOException err) {
             err.printStackTrace();
         }
 
-        myHand = FXCollections.observableArrayList();
-        view.getMyHand().setListViewItems(myHand);
-
-        discarded = FXCollections.observableArrayList();
-        view.getDiscardedCards().setListViewItems(discarded);
-
         // a lot of this is just for laying out gui will be removed later
         view.getCurrentStateText().setText("Your turn!");
         view.getShieldsView().setShields(1);
-
-//        for (int i = 1; i <= 11; i++) {
-//            addCardToHand(myHand, new AllyCard(
-//                    new Image(String.valueOf(getClass().getResource("/specials/quest_ally_" + i + ".png"))),
-//                    "card",
-//                    ""));
-//        }
-//
-//        for (int i = 1; i <= 10; i++) {
-//            CardView cardView = new CardView(new QuestCard(
-//                    new Image(String.valueOf(getClass().getResource("/quests/quest_quest_" + i + ".png"))),
-//                    "card", 3, ""));
-//            discarded.add(0, cardView);
-//        }
 
 
         // set action for draw card button
         view.getDrawCardButton().setOnAction(e -> {
             // draw a card from server
-//            Card drawnCard = new AllyCard(
-//                    new Image(String.valueOf(getClass().getResource("/specials/quest_ally_3.png"))),
-//                    "card",
-//                    "");
+            // hardcoded for testing
+            Card drawnCard = new AllyCard(
+                    "card",
+                    "/specials/quest_ally_3.png",
+                    "");
+
 
             // once hand has more than 12 cards every next card drawn must be either played or discarded
             if (myHand.size() < 12) {
-//                addCardToHand(myHand, drawnCard);
+                addCardToHand(myHand, drawnCard);
             } else {
                 // display card with option to play it or discard it
-//                view.getDrawnCard().setCard(drawnCard);
-//                view.setCenter(view.getDrawnCard());
-//                view.getDrawCardButton().setDisable(true);
+                view.getDrawnCard().setCard(drawnCard);
+                view.setCenter(view.getDrawnCard());
+                view.getDrawCardButton().setDisable(true);
             }
         });
 
@@ -110,28 +96,43 @@ public class GameController {
         view.getShowHandButton().setOnAction(e -> {
             System.out.println("showing hand");
             // show and hide hand
+            clearCardViewButtonsHighlight(view);
             if (view.getBottom() != null && view.getBottom().equals(view.getMyHand().getListView())) {
                 view.setBottom(null);
-                view.getShowHandButton().getStyleClass().remove("caution");
             } else {
                 view.setBottom(view.getMyHand().getListView());
                 view.getShowHandButton().getStyleClass().add("caution");
-                view.getShowDiscardedButton().getStyleClass().remove("caution");
             }
         });
 
         view.getShowDiscardedButton().setOnAction(e -> {
             System.out.println("showing discarded");
             // show and hide discarded
+            clearCardViewButtonsHighlight(view);
             if (view.getBottom() != null && view.getBottom().equals(view.getDiscardedCards().getListView())) {
                 view.setBottom(null);
-                view.getShowDiscardedButton().getStyleClass().remove("caution");
             } else {
                 view.setBottom(view.getDiscardedCards().getListView());
                 view.getShowDiscardedButton().getStyleClass().add("caution");
-                view.getShowHandButton().getStyleClass().remove("caution");
             }
         });
+    }
+
+    private void disableView(GamePane view, Boolean disable) {
+        if (disable) {
+            view.setBottom(null);
+            view.getDeckButtons().setVisible(false);
+            view.getCardButtons().setVisible(false);
+            clearCardViewButtonsHighlight(view);
+        } else {
+            view.getDeckButtons().setVisible(true);
+            view.getCardButtons().setVisible(true);
+        }
+    }
+
+    private void clearCardViewButtonsHighlight(GamePane view) {
+        view.getShowDiscardedButton().getStyleClass().remove("caution");
+        view.getShowHandButton().getStyleClass().remove("caution");
     }
 
     private void addCardToHand(ObservableList<CardView> hand, Card card) {
