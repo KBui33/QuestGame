6d979e2eb114752116604adda016e46ce94564ab
Hilaire Djani <djani@smartbuildingapps.com>
2022-04-06 15:50:22 -0400

    - Added connect error alert for client

--- src/main/java/gui/controllers/ConnectController.java
+++ src/main/java/gui/controllers/ConnectController.java
@@ -1,11 +1,16 @@
 package gui.controllers;
 
 import gui.main.ClientApplication;
+import gui.other.AlertBox;
 import gui.panes.ConnectPane;
 import gui.scenes.LobbyScene;
+import javafx.scene.control.Alert;
 import networking.client.Client;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * @author James DiNovo
@@ -21,16 +26,16 @@ public class ConnectController {
         view.getConnectButton().setOnAction(e -> {
             String serverHost = view.getServerAddress().getText().trim();
             System.out.println("Connecting to " + serverHost + "...");
+
             try {
                 // Create new client instance to connect to server
                 Client client = Client.initialize(serverHost);
                 System.out.println("== Connect Controller says: Loading lobby scene...");
                 ClientApplication.window.setScene(new LobbyScene());
-            } catch(IOException err) {
+            } catch(IOException | ClassNotFoundException err) {
                 // display error to user later on
                 err.printStackTrace();
-            } catch (ClassNotFoundException ex) {
-                ex.printStackTrace();
+                AlertBox.alert("Could not connect to server.\nMake sure there is a game server running.", Alert.AlertType.ERROR);
             }
 
 
